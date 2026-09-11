027a7104889b52ff2eff1b64037ecab5b746baef
Emanoel Caico <emanoeldf@gmail.com>
2026-09-11 10:07:09 -0400

    comid

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 
 print( 'servidor Ok, Agora 11-08-26----2050')
 print( 'servidor Ok, Agora é pra valer')
+print( 'servidor Ok, Agora eu Aprendi a sincronizar o tempo do servidor com o tempo do cliente')
