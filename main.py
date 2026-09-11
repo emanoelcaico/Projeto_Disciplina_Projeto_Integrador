72ec7b801ed9bbcae2a85cd0649ad918afd60ac3
Emanoel Caico <emanoeldf@gmail.com>
2026-09-11 10:17:12 -0400

    99

--- main.py
+++ main.py
@@ -2,3 +2,4 @@
 print( 'servidor Ok, Agora 11-08-26----2050')
 print( 'servidor Ok, Agora é pra valer')
 print( 'servidor Ok, Agora eu Aprendi a sincronizar o tempo do servidor com o tempo do cliente')
+print( 'servidor Ok, ----2050')
